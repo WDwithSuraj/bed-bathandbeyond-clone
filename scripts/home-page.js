8876c621b6c38a0e51488472b421a0c7b3051bf4
Jignesh <jigsvadiyatar6557@gmail.com>
2023-02-24 22:30:57 +0530

    day-4 updated

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -145,3 +145,21 @@ if (userDetails) {
 //    location.href = ''
 // }
 
+
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
